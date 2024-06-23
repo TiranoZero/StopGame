e8e357e0d8ca20561bca6145d250110de4db90d9
Imperador <147494303+TiranoZero@users.noreply.github.com>
2024-06-22 22:10:12 -0300

    add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-from discord import Intents
+from discord import Intents, Member, utils, Embed, File
 from discord.ext import commands
 from decouple import config as key
 
@@ -12,6 +12,19 @@ tocken=key("TK")
 async def on_ready():
   nome = bot.user.name
   banner = f"Bot:{nome}\nStatus:Online"
+
+
+@bot.event
+async def on_member_join(m:Member):
+    nome = m.name
+    channel = utils.get(m.guild.channels, name="entrada")
+    banner = Embed(title="Bem viado",
+                   description="seja bem vindo ao lado de fora",
+                   color=0xFF0000)
+    foto = absolute.abcinema(nome)
+    file = File(foto)
+    banner.set_image(url=f"attachment://{foto}")
+    await channel.send(file=file, embed=banner)
 
 bot.run(tocken)
 
